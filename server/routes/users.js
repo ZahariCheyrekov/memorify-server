db9f90f0d2bd64d4f66e3bfa2da0518c562076e9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 18:11:15 +0200

    Add user routes

---
+++ server/routes/users.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { getUsers, signin, signup } from '../controllers/user.js';
+
+const router = express.Router();
+
+router.get('/', getUsers);
+router.post('/signup', signup);
+router.post('/signin', signin);
+
+export default router;
